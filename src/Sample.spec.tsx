27a6279bfc5209fde94060a70f90871f77e096d5
Ilya Ivanov <static.ila@gmail.com>
2017-11-30 00:16:23 +0200

    Added unit tests. Included configuration for TypeScript with sample snapshot tests

---
+++ src/Sample.spec.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Sample from './Sample';
+
+// Note: test renderer must be required after react-native.
+import {create} from 'react-test-renderer';
+
+it('renders correctly', () => {
+  const tree = create(
+    <Sample type="sample"/>
+  );
+  expect(tree).toMatchSnapshot();
+});
