6a796852510377d872eea8dfca3ae5a2c9241f63
Ilya Ivanov <static.ila@gmail.com>
2017-12-03 21:15:47 +0200

    #12. Fix test after root name change

--- data/models/navigation.spec.ts
+++ data/models/navigation.spec.ts
@@ -4,7 +4,7 @@ import {navigate} from './navigation';
 describe('Having an awesome repository ', () => {
   it('when referencing an empty link shound return a Home section', function () {
     const section = navigate(repository);
-    expect(section.title).toBe('Contents');
+    expect(section.title).toBe('Awesome List');
   });
   it('when referencing a Big Data section should return that section with non-empty links', function () {
     const section = navigate(repository, '#big-data');
